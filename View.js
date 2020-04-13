089103edd0a0a753fd90ee623cd736f3ddc75114
Jeanette <yenia94@hotmail.com>
2020-04-13 13:09:23 -0500

    nuevaFuncion

--- View.js
+++ View.js
@@ -6,3 +6,12 @@ function control(){
     var z = 6;
     console.log (z*x);
 }
+
+function control2(){
+    //cambios de rama
+    var x = 0;
+    x = 5 + 6
+     console.log(x)
+    var z = 6;
+    console.log (z*x);
+}
